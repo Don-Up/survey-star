6a43a60a5204e8bbc0a0faf631822fdc0aee5b9a
Don-Up <yexudong188@gmail.com>
2025-02-02 09:14:12 +0800

    refactor(QuestionnaireParagraph): improve text rendering for multi-line content
    - Split text into separate lines and render with line breaks
    - Enhance readability and visual presentation of paragraph content

--- src/components/QuestionnaireComponents/QuestionnaireParagraph/Component.tsx
+++ src/components/QuestionnaireComponents/QuestionnaireParagraph/Component.tsx
@@ -2,12 +2,22 @@ import React from "react";
 import {QuestionnaireParagraphDefaultProps, QuestionnaireParagraphPropsType} from "./interface";
 import {Typography} from "antd";
 
-const { Paragraph } = Typography
+const {Paragraph} = Typography
 const QuestionnaireParagraph: React.FC<QuestionnaireParagraphPropsType> = (props) => {
-    const { text = "", isCenter = false } = {...QuestionnaireParagraphDefaultProps, ...props}
+    const {text = "", isCenter = false} = {...QuestionnaireParagraphDefaultProps, ...props}
+
+    const textList = text.split("\n")
+
 
     return (<Paragraph style={{textAlign: isCenter ? "center" : "left", marginBottom: 0}}>
-        {text}
+        {
+            textList.map((item, index) => {
+                return <span key={index}>
+                    {index > 0 && <br/>}
+                    {item}
+                </span>
+            })
+        }
     </Paragraph>)
 }
 
