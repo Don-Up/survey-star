2d9b7e482a88642223050fef5baa6e4afc9cb57a
Don-Up <yexudong188@gmail.com>
2025-02-05 07:40:47 +0800

    feat(questionnaire): add page setting and optimize right panel
    - Add PageSetting component for questionnaire edit page
    - Implement TAB_KEYS enum for better tab management- Use useState and useEffect to dynamically change active tab based on selected component
    - Integrate PageSetting component into RightPanel
    - Remove defaultActiveKey from Tabs and use activeKey for dynamic tab control

---
+++ src/pages/questionnaire/edit/PageSetting.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const PageSetting: React.FC = () => {
+    
+    return (<div>
+        PageSetting
+    </div>)
+}
+
+export default PageSetting
--- src/pages/questionnaire/edit/RightPanel.tsx
+++ src/pages/questionnaire/edit/RightPanel.tsx
@@ -1,12 +1,31 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import {FileTextOutlined, SettingOutlined} from "@ant-design/icons";
 import {Tabs} from "antd";
 import ComponentProp from "./ComponentProp";
+import PageSetting from "./PageSetting";
+import useGetComponentInfo from "../../../hook/useGetComponentInfo";
 
+enum TAB_KEYS  {
+    prop = "prop",
+    setting = "setting"
+}
 const RightPanel: React.FC = () => {
+
+    const [activeKey, setActiveKey] = useState(TAB_KEYS.prop)
+    
+    const { selectedId } = useGetComponentInfo()
+
+    useEffect(() => {
+    	if (selectedId) {
+    		setActiveKey(TAB_KEYS.prop)
+    	} else {
+    		setActiveKey(TAB_KEYS.setting)
+    	}
+    }, [selectedId]);
+    
     const tabsItems = [
         {
-            key: "prop",
+            key: TAB_KEYS.prop,
             label: (
                 <span>
                     <FileTextOutlined/>
@@ -16,17 +35,17 @@ const RightPanel: React.FC = () => {
             children: <ComponentProp/>
         },
         {
-            key: "setting",
+            key: TAB_KEYS.setting,
             label: (
                 <span>
                     <SettingOutlined/>
                     Setting
                 </span>
             ),
-            children: <div>Setting</div>
+            children: <PageSetting/>
         }
     ]
-    return (<Tabs defaultActiveKey={"prop"} items={tabsItems}/>)
+    return (<Tabs activeKey={activeKey} items={tabsItems}/>)
 }
 
 export default RightPanel
