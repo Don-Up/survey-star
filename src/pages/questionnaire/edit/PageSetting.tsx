eadd74341726aff36eeaa5d5abbf6921586c07d1
Don-Up <yexudong188@gmail.com>
2025-02-05 09:35:18 +0800

    feat(questionnaire): add page settings functionality
    - Implement PageSetting component with form for editing page information
    - Add useGetPageInfo hook to retrieve page information from Redux store
    - Integrate form with Redux to update page information in real-time
    - Include fields for title, description, and CSS customization

--- src/pages/questionnaire/edit/PageSetting.tsx
+++ src/pages/questionnaire/edit/PageSetting.tsx
@@ -1,10 +1,34 @@
 import React from "react";
+import useGetPageInfo from "../../../hook/useGetPageInfo";
+import {Form, Input} from "antd";
+import {useDispatch} from "react-redux";
+import {resetPageInfo} from "../../../store/pageInfoReducer";
 
 const PageSetting: React.FC = () => {
-    
-    return (<div>
-        PageSetting
-    </div>)
+    const pageInfo = useGetPageInfo()
+    // form
+    const [form] = Form.useForm()
+    // dispatch
+    const dispatch = useDispatch()
+    function handleValuesChange() {
+        dispatch(resetPageInfo(form.getFieldsValue()))
+    }
+
+    return (<Form
+        layout={"vertical"}
+        initialValues={pageInfo}
+        onValuesChange={handleValuesChange}
+    >
+        <Form.Item label={"Title"} name={"title"} rules={[{required: true, message: "Please enter a title."}]}>
+            <Input placeholder="Title"/>
+        </Form.Item>
+        <Form.Item label={"Description"} name={"description"}>
+            <Input.TextArea placeholder="Description"/>
+        </Form.Item>
+        <Form.Item label={"CSS"} name={"css"}>
+            <Input.TextArea placeholder="CSS code"/>
+        </Form.Item>
+    </Form>)
 }
 
 export default PageSetting
